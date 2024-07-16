d27afb897a7ae92cd77c4a216c3246a1efb6e478
Hwang Dong Gi <hdg9999@gmail.com>
2024-07-16 19:24:11 +0900

    README 업데이트, 폴더 등록 관련 오류 방지 처리

--- Service/Service_folderPicker.py
+++ Service/Service_folderPicker.py
@@ -2,22 +2,26 @@ import tkinter
 import os
 import pprint
 import traceback
+from pathlib import Path
 import streamlit as st
 
 from tkinter import filedialog
 from DB.DB_Chroma import DB_CLIENT
 
 def select_folder():
-   root = tkinter.Tk()
-   root.withdraw()
-   folder_path = filedialog.askdirectory(master=root)
-   root.destroy()
+   tk = tkinter.Tk()
+   tk.wm_attributes("-topmost",1)
+   tk.withdraw()
+   folder_path = filedialog.askdirectory(master=tk)
+   tk.destroy()
    return folder_path
 
 def upload():
-   ids = [file.path for file in os.scandir(st.session_state.folder_path)]
-   image_files = [file.path for file in os.scandir(st.session_state.folder_path)]
-   metadatas = [{'file_name':file.name} for file in os.scandir(st.session_state.folder_path)]
+   allowed_exts = ['.jpg','.jpeg','.png','.gif','.webp']
+   target_files = [ file for file in os.scandir(st.session_state.folder_path) if Path(file.name).suffix in allowed_exts ]
+   ids = [ file.path for file in target_files ]
+   image_files = [ file.path for file in target_files ]
+   metadatas = [ {'file_name':file.name} for file in target_files ]
 
    DB_CLIENT.get_or_create('img')
    DB_CLIENT.add('img',ids=ids, image_files=image_files, file_info=metadatas)
